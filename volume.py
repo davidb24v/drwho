7843a72d15e5bb349a9f9e11ebf9d2b60572af92
Dave Boulton <davidb24v@gmail.com>
2014-03-11 18:26:16 +0000

    Set permission on Level.txt

--- volume.py
+++ volume.py
@@ -15,10 +15,10 @@ class Volume(object):
         self.setVolume(create=True)
 
     def setVolume(self, create=False):
-        file = os.path.join(self.wd, "Sounds", "Volume", "Level.txt")
-        if glob.glob(file):
+        volFile = os.path.join(self.wd, "Sounds", "Volume", "Level.txt")
+        if glob.glob(volFile):
             try:
-                vol = open(file, 'rb')
+                vol = open(volFile, 'rb')
                 data = vol.readline().strip()
                 volume = int(data)
             except:
@@ -34,9 +34,10 @@ class Volume(object):
                     except:
                         pass
                 try:
-                    vol = open(file, "wb")
+                    vol = open(volFile, "wb")
                     vol.write(str(volume) + "\n")
                     vol.close()
+                    subprocess.call(['chown', 'pi.pi', volFile])
                 except:
                     pass
         subprocess.call(["amixer", "set", "PCM", "{}%".format(volume)])
